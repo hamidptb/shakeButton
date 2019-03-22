5b56dbc2caf8aa47c5ca6f776a050e7212aa2537
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2019-03-22 13:28:59 +0430

    Initial Commit

---
+++ shakeButton/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  shakeButton
+//
+//  Created by Hamid Reza Ansari on 3/21/19.
+//  Copyright © 2019 Hamid Reza Ansari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnOut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func btnAct(_ sender: Any) {
+        
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: btnOut.center.x - 5, y: btnOut.center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: btnOut.center.x + 5, y: btnOut.center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        btnOut.layer.add(shake, forKey: "position")
+        
+    }
+    
+}
+
